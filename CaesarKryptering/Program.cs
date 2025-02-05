7a45ae5fd555917fe71fe4452867a9f455e5c130
Bjoern <bjoe6880@elev.techcollege.dk>
2025-02-05 09:47:57 +0100

    Add project files.

---
+++ CaesarKryptering/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace CaesarKryptering
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til Cæsar krypteringsprogram");
+            Console.Write("For kryptering tast 1, for dekryptering tast 2: ");
+            ConsoleKeyInfo cki = Console.ReadKey();
+            Console.WriteLine("");
+            Console.WriteLine("----------------------------------------------");
+
+            if (cki.Key == ConsoleKey.D1 || cki.Key == ConsoleKey.NumPad1)
+            {
+                Console.WriteLine("Indtast din besked der skal krypteres: ");
+                string input = Console.ReadLine();
+                Console.WriteLine("Indtast nøgle: ");
+                string inputShift = Console.ReadLine();
+                int shift = int.Parse(inputShift);
+                
+                string result = EncryptText(input, shift);
+                Console.WriteLine($"Krypteret text med {shift} skift: {result}");
+
+            }
+            else if (cki.Key == ConsoleKey.D2 || cki.Key == ConsoleKey.NumPad2)
+            {
+                Console.WriteLine("Indtast din besked der skal dekrypteres: ");
+                string input = Console.ReadLine();
+                Console.WriteLine("Indtast nøgle: ");
+                string inputShift = Console.ReadLine();
+                int shift = int.Parse(inputShift);
+
+                string result = DecryptText(input, shift);
+                Console.WriteLine($"Dekrypteret text med {shift} skift: {result}");
+
+            }
+            else
+            {
+                Console.WriteLine("Ugyldig input!");
+            }
+
+        }
+
+        public static string EncryptText(string input, int shift)
+        {
+            string result = "";
+
+            foreach (char c in input.ToUpper()) //lav alle bogstaver om til stor
+            {
+                if (char.IsLetter(c) && "ÆØÅ".IndexOf(c) == -1) //Frasort ikke bostaver og bogstaver som æ, ø, å
+                {
+                    char offset = 'A';      //sæt start til wrap around
+                    result += (char)(((c - offset + shift) % 26) + offset); 
+                }
+                else
+                {
+                    result += c; // behold ikke bostaver som de er
+                }
+            }
+
+            return result;
+
+        }
+
+        public static string DecryptText(string input, int shift)
+        {
+            string result = "";
+
+            foreach (char c in input.ToUpper()) //lav alle bogstaver om til stor
+            {
+                if (char.IsLetter(c) && "ÆØÅ".IndexOf(c) == -1) //Frasort ikke bostaver og bogstaver som æ, ø, å
+                {
+                    char offset = 'A';      //sæt start til wrap around
+                    result += (char)(((c - offset - shift) % 26) + offset);
+                }
+                else
+                {
+                    result += c; // behold ikke bostaver som de er
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ UnitTestCasesar/Test1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Runtime.CompilerServices;
+
+namespace UnitTestCasesar
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        private const string Expected1 = "BCDE";
+        private const string Expected2 = "ABCD";
+
+        [TestMethod]
+        public void TestEncryptText()
+        {
+            // Arrange
+            string input = "ABCD";
+            int shift = 1;
+
+            // Act
+            string result = CaesarKryptering.Program.EncryptText(input, shift);
+
+            // Assert
+            Assert.AreEqual(Expected1, result, "Encryption failed");
+        }
+
+        [TestMethod]
+        public void TestDecryptText()
+        {
+            // Arrange
+            string input = "BCDE";
+            int shift = 1;
+
+            // Act
+            string result = CaesarKryptering.Program.DecryptText(input, shift);
+
+            // Assert
+            Assert.AreEqual(Expected2, result, "Decryption failed");
+        }
+
+        [TestMethod]
+        public void TestEncryptText2()
+        {
+            // Arrange
+            string input = "ABCD";
+            int shift = 1;
+
+            // Act
+            string result = CaesarKryptering.Program.EncryptText(input, shift);
+
+            // Assert
+            Assert.AreNotEqual(Expected2, result, "Decryption failed");
+        }
+
+        [TestMethod]
+        public void TestDecryptText2()
+        {
+            // Arrange
+            string input = "ABCD";
+            int shift = 1;
+
+            // Act
+            string result = CaesarKryptering.Program.DecryptText(input, shift);
+
+            // Assert
+            Assert.AreNotEqual(Expected1, result, "Decryption failed");
+        }
+    }
+}
